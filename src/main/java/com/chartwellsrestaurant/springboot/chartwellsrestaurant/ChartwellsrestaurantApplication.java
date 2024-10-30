f5e9b60b775fc274c585b932bc59447e823b9b24
Ravindra <ravindra1s.siddavatam@gmail.com>
2024-10-29 19:53:22 -0500

    first commit

---
+++ src/main/java/com/chartwellsrestaurant/springboot/chartwellsrestaurant/ChartwellsrestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.chartwellsrestaurant.springboot.chartwellsrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChartwellsrestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChartwellsrestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chartwellsrestaurant/springboot/chartwellsrestaurant/ChartwellsrestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chartwellsrestaurant.springboot.chartwellsrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChartwellsrestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
